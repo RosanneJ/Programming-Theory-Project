71cf775aa0320e1988351478affca34a2ed3149b
Rosanne Joosten <rosanne.joosten@hotmail.com>
2022-04-13 14:46:39 +0200

    initial player controls
    Although I trip over myself XD

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,10 @@
+﻿public static class PlayerControl
+{
+    public static string Forward = "w";
+    public static string Backward = "s";
+    public static string Left = "a";
+    public static string Right = "d";
+
+    public static string Interact = "shift";
+    public static string Jump = "space";
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,22 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody m_Rigidbody;
+    public float Speed;
+    
     void Start()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateMovement();
+        UpdateViewDirection();
+        UpdatePosition();
     }
 
-    private void UpdateMovement()
+    private void UpdateViewDirection()
     {
-        float x = 5 * Input.GetAxis("Mouse X");
-        float y = 5 * Input.GetAxis("Mouse Y");
-        transform.Rotate(x, y, 0);
+        float y = 5 * Input.GetAxis("Mouse X");
+        transform.Rotate(0, y, 0);
+    }
+
+    private void UpdatePosition()
+    {
+        if (Input.GetKeyDown(PlayerControl.Forward))
+        {
+            m_Rigidbody.velocity = transform.forward * Speed;
+        } else if (Input.GetKeyDown(PlayerControl.Backward))
+        {
+            m_Rigidbody.velocity = -transform.forward * Speed;
+        }
+
+        if (Input.GetKeyDown(PlayerControl.Left))
+        {
+            m_Rigidbody.velocity = -transform.right * Speed;
+        } else if (Input.GetKeyDown(PlayerControl.Right))
+        {
+            m_Rigidbody.velocity = transform.right * Speed;
+        }
     }
 }
