5701d8704c5bf945246c31282b5f5843647c7faf
Rosanne Joosten <rosanne.joosten@hotmail.com>
2022-04-22 11:49:02 +0200

    Add first moving target

---
+++ Assets/Scripts/WalkingGarden.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
+
+public class WalkingGarden : MonoBehaviour
+{
+    public float MovingSpeed = 1f;
+
+    public float DistanceBeforeTurn = 4f;
+
+    private Rigidbody _rb;
+
+    private bool _changeDirection = true;
+    private Vector3 _previousPosition;
+    private Quaternion _targetDirection;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move je kontje
+        _rb.velocity = transform.forward * MovingSpeed * Time.deltaTime;
+        
+        if (_changeDirection)
+        {
+            // save starting position
+            _previousPosition = transform.position;
+            
+            // turn object in random direction
+            float randomDirection = Random.Range(0, 360);
+            _targetDirection = Quaternion.Euler(0, randomDirection, 0);
+            transform.rotation = _targetDirection;
+        }
+
+        // check if distance before turn is reached
+        _changeDirection = Vector3.Distance(_previousPosition, transform.position) >= DistanceBeforeTurn;
+    }
+}
